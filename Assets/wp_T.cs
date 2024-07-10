8641166ed5629ae12df6ed5ac18f2b7cf267484b
KoheiYamashita23 <221205179@ccmailg.meijo-u.ac.jp>
2024-07-10 14:43:53 +0900

    ワープ

---
+++ Assets/wp_T.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wp_T : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // （考え方）触れた瞬間にボールに新しい位置情報をセットする。
+        // 「0.5f」のように「小数」を使用する場合には必ず「f」を書くこと（ポイント）
+        // 「f」は「float（浮動小数点）」の略
+        other.gameObject.transform.position = new Vector3(1.0f,27.71f ,2.63f );
+    }
+}
